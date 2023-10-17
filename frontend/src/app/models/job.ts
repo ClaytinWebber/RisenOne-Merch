b5c583214e13bcab3f26a8c418ed93ecfeec3755
ClayDill <101902627+ClayDill@users.noreply.github.com>
2023-10-17 18:23:31 -0500

    exampleperge

--- frontend/src/app/models/job.ts
+++
@@ -1,8 +0,0 @@
-export interface Job {
-    id: string;
-    title: string;
-    description: string;
-    companyName: string;
-    location: string;
-    level: string;
-}
--- frontend/src/app/shared/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-export class Constants {
-  public static ADMIN_ROUTES = {
-    JOBS: '/jobs',
-  };
-
-  public static APIS = {
-    JOBS: '/board/jobs',
-  };
-}
